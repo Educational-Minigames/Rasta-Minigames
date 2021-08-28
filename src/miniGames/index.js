13cb1c06393e4ec2120ad817f016630332c1de6e
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-08-28 20:03:57 +0430

    init signal second game

--- src/miniGames/index.js
+++ src/miniGames/index.js
@@ -9,7 +9,8 @@ import ImageProcessing3 from './image_processing3';
 // Physics workshop
 import Physic_collision from './Physic_collision';
 // signal workshop
-import Signal from './signal1';
+import Signal1 from './signal1';
+import Signal2 from './signal2';
 
 const MINI_GAMES = [
   // Image processing games
@@ -81,23 +82,44 @@ const MINI_GAMES = [
 
   // Signal games
   {
-    component: Signal,
+    component: Signal1,
     props: { sound_file: 'Noise.wav', duration: 1.22 },
     url: '/signal1-1',
     name: 'بازی فوریه ۱-۱',
   },
   {
-    component: Signal,
+    component: Signal1,
     props: { sound_file: 'Noisy.wav', duration: 1.22 },
     url: '/signal1-2',
     name: 'بازی فوریه ۱-۲',
   },
   {
-    component: Signal,
+    component: Signal1,
     props: { sound_file: 'Main.wav', duration: 1.22 },
     url: '/signal1-3',
     name: 'بازی فوریه ۱-۳',
   },
+
+  {
+    component: Signal2,
+    props: { sound_file: 'Noise.wav', duration: 1.22 },
+    url: '/signal2-1',
+    name: 'بازی سیگنال ۲-۱',
+  },
+  {
+    component: Signal2,
+    props: { sound_file: 'Noisy.wav', duration: 1.22 },
+    url: '/signal2-2',
+    name: 'بازی سیگنال ۲-۲',
+  },
+  {
+    component: Signal2,
+    props: { sound_file: 'Main.wav', duration: 1.22 },
+    url: '/signal2-3',
+    name: 'بازی سیگنال ۲-۳',
+  },
+
+
 
   // Bio games
   {
