79d519b2a940ec6b42ccc8652e3b8612f15bf270
f.labbaf98 <f.labbaf98@gmail.com>
2021-08-29 21:11:36 +0430

    add Ligic table2 game

---
+++ src/miniGames/Logic_table2/index.js
@@ -0,0 +1,27 @@
+import { makeStyles } from '@material-ui/core';
+import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  div: {
+    height: '100vh',
+    width: '100vw',
+  },
+  iframe: {
+    height: '100%',
+    width: '100%',
+    border: '0px',
+  },
+}));
+
+function index() {
+  const classes = useStyles();
+  return (
+    <div className={classes.div}>
+      <iframe className={classes.iframe} title='Sine_waves' src={`${process.env.PUBLIC_URL}/MiniGames/Sine_Waves/index.html`} />
+    </div>
+  );
+}
+
+export default index;
+
+
